62394edddb35aff8b741198bd814c55f6ce5c584
Gianluca Mazzini <gianluca@mazzini.org>
2020-10-24 07:22:55 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<h1>Really simple immediate service by IK4LZH</h1>
+
+<form action="cqwwdigi.php" method="post" enctype="multipart/form-data">
+<pre>CQWWDIGI Score computation from cabrillo <input type="file" name="cbrfile"><input type="submit" name="submit" value="Upload"></pre>
+</form>
+
+<form action="country.php" method="post" enctype="multipart/form-data">
+<pre>Country identificaton (callsign) <input type="text" name="call"> <input type="submit" name="submit" value="Submit"></pre>
+</form>
+
+<form action="password.php" method="post" enctype="multipart/form-data">
+<pre>Password Generator (len) <input type="text" name="passlen"><input type="submit" name="submit" value="Submit">
+</pre>
+</form>
+
+</pre>
