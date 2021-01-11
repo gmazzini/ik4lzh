702e0c76c286e4677ca397632a59c1bbf03b418e
Gianluca Mazzini <gianluca@mazzini.org>
2021-01-11 16:24:20 +0100

    Add files via upload

---
+++ download.php
@@ -0,0 +1,18 @@
+<?php
+
+for($i=0;$i<1000;$i+=30){
+  $aux=file_get_contents("http://maxlaconca.com/mdxc_directory/lista_ita.php?startrow=$i");
+  $pp=explode("<tr>",$aux);
+  foreach ($pp as &$vv){
+    $qq=explode("</td>",$vv);
+    if(isset($qq[3])){
+      $qq1=strrpos($qq[0],">");
+      $myid=(int)substr($qq[0],$qq1+1);
+      $qq2=strrpos($qq[3],">");
+      $mycall=substr($qq[3],$qq2+1);
+      if($myid>0&&strlen($mycall)>2)echo "$myid,$mycall\n";
+    }
+  }
+}
+
+?>
