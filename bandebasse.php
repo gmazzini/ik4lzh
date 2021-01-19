6879bb2fb7558ea86efb85bf1940082fead1efbc
Gianluca Mazzini <gianluca@mazzini.org>
2021-01-19 21:24:45 +0100

    Update bandebasse.php

--- bandebasse.php
+++ bandebasse.php
@@ -18,65 +18,30 @@ while(!feof($hh)){
   if($aux=="IQ"||$aux=="IY")$pp=10;
   if(!isset($point[$myid]))$point[$myid]=$pp;
  
-  
   $myid=$band."-".$parts[2]."-".substr($parts[10],0,2);
   if(!isset($mult[$myid]))$mult[$myid]=1;
-  
   $mdxc=(int)substr($parts[10],3);
-  if($mdxc>0)
+  if($mdxc>0){
+    $myid=$band."-".$parts[2]."!".$mdxc;
+    if(!isset($mult[$myid]))$mult[$myid]=1;  
+  }
   
   $myid=$band."!".(int)$parts[10];
   if(!isset($mult[$myid]))$mult[$myid]=1;
   if(!isset($myband[$band]))$myband[$band]=1;
-  
-  
-  
-    
-  $prov=$parts[10+$member];
-  if(isset($parts[11+$member]))$mdxc=(int)$parts[11+$member];
-  else $mdxc=0;
-  
-  $ff=strpos($call,"/");
-  if($ff===false)$scall=$call;
-  else $scall=substr($call,0,$ff);
-  if(isset($mymdxc[$scall]))$cmdxc=$mymdxc[$scall];
-  else $cmdxc=0;
-    
-  
-    
-
-  $c_molt=$prov."-".$myid;
-  if(!isset($w_molt[$c_molt])){
-    $w_molt[$c_molt]=1;
-    if(!isset($mult_p[$myid]))$mult_p[$myid]=1;
-    else $mult_p[$myid]++;
-  }
-
-  if($mdxc!=0){
-    $c_molt=$mdxc."-".$myid;
-    if(!isset($w_molt[$c_molt])){
-      $w_molt[$c_molt]=1;
-      if(!isset($mult_m[$myid]))$mult_m[$myid]=1;
-      else $mult_m[$myid]++;
-    }
-  }
 }
 fclose($hh);
 
-$keys=array_keys($qso);
-sort($keys);
-
-echo "\tQSO\tPOINT\tM_PROV\tM_MDXC\n";
-$vqso=0; $vpoint=0; $vmult_p=0; $vmult_m=0;
-foreach ($keys as &$k) {
-  echo "$k\t$qso[$k]\t$point[$k]\t$mult_p[$k]\t$mult_m[$k]\n";
-  $vqso+=$qso[$k];
-  $vpoint+=$point[$k];
-  $vmult_p+=$mult_p[$k];
-  $vmult_m+=$mult_m[$k];
+echo "<pre>\n";
+echo "BAND\tQSOs\tPOINTs\tM_CYs\tM_CQs\n";
+$ea=array_keys($myband);
+sort($ea);
+foreach($ea as $ee){
+  echo $ee."\t";
+  echo mysum($qso,"-",$ee)."\t";
+  echo mysum($point,"-",$ee)."\t";
+  echo mysum($mult,"-",$ee)."\t";
+  echo mysum($mult,"!",$ee)."\n";
 }
-echo "TOTAL\t$vqso\t$vpoint\t$vmult_p\t$vmult_m\n";
-$tot=$vpoint*($vmult_p+$vmult_m);
-echo "SCORE: $tot\n";
-echo "</pre>";
+echo $mycall." SCORE: ".array_sum($point)*array_sum($mult)."\n";
 ?>
