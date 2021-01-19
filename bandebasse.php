31753b78b835cf493eceeadad8abb02aa68c7f68
Gianluca Mazzini <gianluca@mazzini.org>
2021-01-19 19:32:26 +0100

    Update bandebasse.php

--- bandebasse.php
+++ bandebasse.php
@@ -1,26 +1,37 @@
 <?php
-
 // v3 by IK4LZH
-// score for bandebasse
+include("utility.php");
 
-echo "<pre>";
-$bb=array(1=>160,3=>80,7=>40);
-eval("\$mymdxc=".file_get_contents("/home/www/ik4lzh.mazzini.org/mdxc.list").";");
-
-$hh=fopen($_FILES['cbrfile']['tmp_name'],"r");
-$member=0;
+if(isset($_FILES['cbrfile']['tmp_name']))$hh=fopen($_FILES['cbrfile']['tmp_name'],"r");
+else $hh=fopen("php://stdin","r");
 while(!feof($hh)){
   $line=fgets($hh);
-  if(substr($line,0,23)!="CATEGORY-MEMBER: MEMBER")$member=1;
   if(substr($line,0,4)!="QSO:")continue;
-  $parts=preg_split('/\s+/',$line);
-  $freq=substr($parts[1],0,strlen($parts[1])-3);
+  $parts=mysep($line,0);
   
-  $call=$parts[8+$member];
-  $freq=substr($parts[1],0,1);
-  $mode=$parts[2];
-  $data=$parts[3];
-  $time=$parts[4];
+  $band=$bb[floor($parts[1]/1000)];
+  $myid=$band."-".$parts[2]."-".$parts[8];
+  if(!isset($qso[$myid]))$qso[$myid]=1;
+  if($parts[2]=="PH")$pp=1;
+  else if($parts[2]=="CW")$pp=2;
+  $aux=substr($parts[8],0,2);
+  if($aux=="IQ"||$aux=="IY")$pp=10;
+  if(!isset($point[$myid]))$point[$myid]=$pp;
+ 
+  
+  $myid=$band."-".$parts[2]."-".substr($parts[10],0,2);
+  if(!isset($mult[$myid]))$mult[$myid]=1;
+  
+  $mdxc=(int)substr($parts[10],3);
+  if($mdxc>0)
+  
+  $myid=$band."!".(int)$parts[10];
+  if(!isset($mult[$myid]))$mult[$myid]=1;
+  if(!isset($myband[$band]))$myband[$band]=1;
+  
+  
+  
+    
   $prov=$parts[10+$member];
   if(isset($parts[11+$member]))$mdxc=(int)$parts[11+$member];
   else $mdxc=0;
@@ -30,24 +41,9 @@ while(!feof($hh)){
   else $scall=substr($call,0,$ff);
   if(isset($mymdxc[$scall]))$cmdxc=$mymdxc[$scall];
   else $cmdxc=0;
-  if($mdxc==0 && $cmdxc>0)echo "$call-$data-$time MDXC not specified\n";
-  else if($mdxc>0 && $cmdxc==0)echo "$call-$data-$time MDXC not more existing\n";
-  else if($mdxc!=$cmdxc)echo "$call-$data-$time MDXC wrong\n";
+    
   
-  $myid=$bb[$freq]."-".$mode;
-  $c_call=$call."-".$myid;
-  if(!isset($w_call[$c_call])){
-    if(!isset($qso[$myid]))$qso[$myid]=1;
-    else $qso[$myid]++;
-    $w_call[$c_call]=1;
-    $myinc=0;
-    if($mode=="PH")$myinc=1;
-    if($mode=="CW")$myinc=2;
-    $aux=substr($call,0,2);
-    if($aux=="IQ"||$aux=="IY")$myinc=10;
-    if(!isset($point[$myid]))$point[$myid]=$myinc;
-    else $point[$myid]+=$myinc;
-  }
+    
 
   $c_molt=$prov."-".$myid;
   if(!isset($w_molt[$c_molt])){
