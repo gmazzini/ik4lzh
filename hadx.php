b681b4f8a36162bb3b67f9af047c7a8be672b1ff
Gianluca Mazzini <gianluca@mazzini.org>
2021-01-19 23:36:49 +0100

    Update hadx.php

--- hadx.php
+++ hadx.php
@@ -37,17 +37,19 @@ while(!feof($hh)){
 fclose($hh);
 
 echo "<pre>\n";
-echo "BAND\tQSOs\tPOINTs\tM_CYs\tM_PRs\n";
+echo "BAND\tQSOSSBs\tQSOCQs\tPOINTSSBs\tPOINTCWs\tM_CYs\tM_PRs\n";
 $ea=array_keys($myrep);
 natsort($ea);
-$z1=$z2=$z3=$z4=0;
+$z1=$z2=$z3=$z4=$z5=$z6=0;
 foreach($ea as $ee){
   echo $ee."\t";
-  $xx=mysum($qso,"-",$ee); $z1+=$xx; echo $xx."\t";
-  $xx=mysum($point,"-",$ee); $z2+=$xx; echo $xx."\t";
-  $xx=mysum($mult,"-",$ee); $z3+=$xx; echo $xx."\t";
-  $xx=mysum($mult,"!",$ee); $z4+=$xx; echo $xx."\n";
+  $xx=mysum($qso,"-",$ee."-PH"); $z1+=$xx; echo $xx."\t";
+  $xx=mysum($qso,"-",$ee."-CW"); $z2+=$xx; echo $xx."\t";
+  $xx=mysum($point,"-",$ee."-PH"); $z3+=$xx; echo $xx."\t";
+  $xx=mysum($point,"-",$ee."-CW"); $z4+=$xx; echo $xx."\t";
+  $xx=mysum($mult,"-",$ee); $z5+=$xx; echo $xx."\t";
+  $xx=mysum($mult,"!",$ee); $z6+=$xx; echo $xx."\n";
 }
-echo "TOTAL\t$z1\t$z2\t$z3\t$z4\n";
+echo "TOTAL\t$z1\t$z2\t$z3\t$z4\t$z5\t$z6\n";
 echo $parts[5]." SCORE: ".array_sum($point)*array_sum($mult)."\n";
 ?>
