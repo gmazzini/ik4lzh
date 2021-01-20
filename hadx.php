f7f86b46b17a2d65fe3d3985b5c6bf762ccfd099
Gianluca Mazzini <gianluca@mazzini.org>
2021-01-20 22:18:39 +0100

    Update hadx.php

--- hadx.php
+++ hadx.php
@@ -1,5 +1,5 @@
 <?php
-// v1 by IK4LZH 20210119
+// v2 by IK4LZH 20210120
 
 include("utility.php");
 $base=1;
@@ -8,29 +8,46 @@ else $hh=fopen("php://stdin","r");
 while(!feof($hh)){
   $line=fgets($hh);
   if(substr($line,0,4)!="QSO:")continue;
-  $parts=mysep($line,0);
+  $parts=mysep($line,10);
   if($base){
     $base=0;
     $mys=findcall($parts[5]);
     $mycont=$mys["cont"];
   }
   
+  $mytt=$parts[3].":".$parts[4];
   $band=$bb[floor($parts[1]/1000)];
   $mys=findcall($parts[8]);
   $myid=$band."-".$parts[2]."-".$parts[8];
-  if(!isset($qso[$myid]))$qso[$myid]=1;  
+  if(!isset($qso[$myid])){
+    $qso[$myid]=1;
+    if(!isset($aqso[$mytt]))$aqso[$mytt]=1;
+    else $aqso[$mytt]++;
+  }
   if($mys["base"]=="HA")$pp=10;
   else if($mys["cont"]!=$mycont)$pp=5;
   else $pp=2;
-  if(!isset($point[$myid]))$point[$myid]=$pp;
+  if(!isset($point[$myid])){
+    $point[$myid]=$pp;
+    if(!isset($apoint[$mytt]))$apoint[$mytt]=$pp;
+    else $apoint[$mytt]+=$pp;
+  }
    
   if($mys["base"]!="HA"){
     $myid=$band."-".$mys["base"];
-    if(!isset($mult[$myid]))$mult[$myid]=1;
+    if(!isset($mult[$myid])){
+      $mult[$myid]=1;
+      if(!isset($amult[$mytt]))$amult[$mytt]=1;
+      else $amult[$mytt]++;
+    }
   }
   else {
     $myid=$band."!".$parts[10];
-    if(!isset($mult[$myid]))$mult[$myid]=1;
+    if(!isset($mult[$myid])){
+      $mult[$myid]=1;
+      if(!isset($amult[$mytt]))$amult[$mytt]=1;
+      else $amult[$mytt]++;
+    }
   }
   if(!isset($myrep[$band]))$myrep[$band]=1;
 }
@@ -51,5 +68,15 @@ foreach($ea as $ee){
   $xx=mysum($mult,"!",$ee); $z6+=$xx; echo $xx."\n";
 }
 echo "TOTAL\t$z1\t$z2\t$z3\t$z4\t$z5\t$z6\n";
-echo $parts[5]." SCORE: ".array_sum($point)*array_sum($mult)."\n";
+echo "\n".$parts[5]." SCORE: ".array_sum($point)*array_sum($mult)."\n\n";
+
+$myd=array_unique(array_keys($aqso));
+sort($myd);
+$z1=$z2=$z3=0;
+foreach($myd as $dd){
+  $z1+=$aqso[$dd];
+  if(isset($apoint[$dd]))$z2+=$apoint[$dd];
+  if(isset($amult[$dd]))$z3+=$amult[$dd];
+  echo $dd.",".$z1.",".$z2.",".$z3."\n";
+}
 ?>
