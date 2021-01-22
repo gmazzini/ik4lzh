68d60cf3434082f10bd19f15acaf79394a290de9
Gianluca Mazzini <gianluca@mazzini.org>
2021-01-22 18:04:50 +0100

    Update 4080.php

--- 4080.php
+++ 4080.php
@@ -1,5 +1,5 @@
 <?php
-// v1 by IK4LZH 20210121
+// v2 by IK4LZH 20210122
 include("utility.php");
 
 if(isset($_FILES['cbrfile']['tmp_name']))$hh=fopen($_FILES['cbrfile']['tmp_name'],"r");
@@ -65,4 +65,5 @@ foreach($myd as $dd){
   if(isset($amult[$dd]))$z3+=$amult[$dd];
   echo $dd.",".$z1.",".$z2.",".$z3."\n";
 }
+file_put_contents("/home/www/ik4lzh.mazzini.org/log.txt",date("Y-m-d H:i:s").",4080,".$parts[5].",".$parts[3].",".$z1.",".$z2.",".$z3."\n", FILE_APPEND | LOCK_EX);
 ?>
