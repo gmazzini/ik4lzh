8255b497f8ad981fa44f439b9714129c47d277b6
Gianluca Mazzini <gianluca@mazzini.org>
2021-01-24 10:57:06 +0100

    Create ubadx.php

---
+++ ubadx.php
@@ -0,0 +1,69 @@
+<?php
+// v1 by IK4LZH 20210124
+
+include("utility.php");
+$base=1;
+if(isset($_FILES['cbrfile']['tmp_name']))$hh=fopen($_FILES['cbrfile']['tmp_name'],"r");
+else $hh=fopen("php://stdin","r");
+while(!feof($hh)){
+  $line=fgets($hh);
+  if(substr($line,0,4)!="QSO:")continue;
+  $parts=mysep($line,10);
+  if($base){
+    $base=0;
+    $mys=findcall($parts[5]);
+    $mybase=$mys["base"];
+    $mycont=$mys["cont"];
+  }
+  
+  $mytt=$parts[3].":".$parts[4];
+  $band=$bb[floor($parts[1]/1000)];
+  $mys=findcall($parts[8]);
+  $myid=$band."-".$parts[8];
+  if(!isset($qso[$myid])){
+    $qso[$myid]=1;
+    if(!isset($aqso[$mytt]))$aqso[$mytt]=1;
+    else $aqso[$mytt]++;
+  }
+  if($mys["cont"]!=$mycont)$pp=3;
+  else if($mys["cont"]=="NA" && $mycont=="NA" && $mys["base"]!=$mybase)$pp=2;
+  else if($mys["cont"]==$mycont && $mys["base"]!=$mybase)$pp=1;
+  else $pp=0;
+  if(!isset($point[$myid])){
+    $point[$myid]=$pp;
+    if(!isset($apoint[$mytt]))$apoint[$mytt]=$pp;
+    else $apoint[$mytt]+=$pp;
+  }
+ 
+  $myid=$band."-".$mys["base"];
+  if(!isset($mult[$myid])){
+    $mult[$myid]=1;
+    if(!isset($amult[$mytt]))$amult[$mytt]=1;
+    else $amult[$mytt]++;
+  }
+  $myid=$band."!".(int)$parts[10];
+  if(!isset($mult[$myid])){
+    $mult[$myid]=1;
+    if(!isset($amult[$mytt]))$amult[$mytt]=1;
+    else $amult[$mytt]++;
+  }
+  if(!isset($myrep[$band]))$myrep[$band]=1;
+}
+fclose($hh);
+
+echo "<pre>\n";
+echo "BAND\tQSOs\tPOINTs\tM_CYs\tM_CQs\n";
+$ea=array_keys($myrep);
+natsort($ea);
+$z1=$z2=$z3=$z4=0;
+foreach($ea as $ee){
+  echo $ee."\t";
+  $xx=mysum($qso,"-",$ee); $z1+=$xx; echo $xx."\t";
+  $xx=mysum($point,"-",$ee); $z2+=$xx; echo $xx."\t";
+  $xx=mysum($mult,"-",$ee); $z3+=$xx; echo $xx."\t";
+  $xx=mysum($mult,"!",$ee); $z4+=$xx; echo $xx."\n";
+}
+echo "TOTAL\t$z1\t$z2\t$z3\t$z4\n";
+echo "\n".$parts[5]." SCORE: ".array_sum($point)*array_sum($mult)."\n\n";
+mybreakdown("ubadx",$parts[5],$parts[3],$aqso,$apoint,$amult);
+?>
