44ba90e17a6055fb010cc8e9f036465d74ec6c9c
Gianluca Mazzini <gianluca@mazzini.org>
2021-01-24 06:28:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,56 +9,54 @@ label {
 </head>
 
 <h1>Online Services by IK4LZH</h1>
-<label>
-Score and breakdown contest computations from cabrillo file, 
-for prostprocessing and analysis, by IK4LZH gianluca@mazzini.org
-</label>
+<label>Score and breakdown contest computations from cabrillo file, 
+for prostprocessing and analysis, by IK4LZH gianluca@mazzini.org</label>
 
 <form action="cqww.php" method="post" enctype="multipart/form-data"> 
-<label>1. CQWW       Score from cabrillo</label>
+<label>01. CQWW       </label>
 <input type="file" name="cbrfile">
 <input type="submit" name="submit" value="Upload">
 </form>
 
 <form action="cqwpx.php" method="post" enctype="multipart/form-data">
-<label>2. CQWPX      Score from cabrillo</label>
+<label>02. CQWPX      </label>
 <input type="file" name="cbrfile">
 <input type="submit" name="submit" value="Upload">
 </form>
 
 <form action="cqwwdigi.php" method="post" enctype="multipart/form-data">
-<label>3. CQWWDIGI   Score from cabrillo</label>
+<label>03. CQWWDIGI   </label>
 <input type="file" name="cbrfile">
 <input type="submit" name="submit" value="Upload">
 </form>
 </p>
 
 <form action="iaruhf.php" method="post" enctype="multipart/form-data">
-<label>4. IARU HF    Score from cabrillo</label>
+<label>04. IARU HF    </label>
 <input type="file" name="cbrfile">
 <input type="submit" name="submit" value="Upload">
 </form>
 
 <form action="hadx.php" method="post" enctype="multipart/form-data">
-<label>5. HADX       Score from cabrillo</label>
+<label>05. HADX       </label>
 <input type="file" name="cbrfile">
 <input type="submit" name="submit" value="Upload">
 </form>
 
 <form action="spdx.php" method="post" enctype="multipart/form-data">
-<label>6. SPDX       Score from cabrillo</label>
+<label>06. SPDX       </label>
 <input type="file" name="cbrfile">
 <input type="submit" name="submit" value="Upload">
 </form>
 
 <form action="bandebasse.php" method="post" enctype="multipart/form-data">
-<label>7. BANDEBASSE Score from cabrillo</label>
+<label>07. BANDEBASSE </label>
 <input type="file" name="cbrfile">
 <input type="submit" name="submit" value="Upload">
 </form>
 
 <form action="4080.php" method="post" enctype="multipart/form-data">
-<label>8. 4080       Score from cabrillo</label>
+<label>08. 4080       </label>
 <input type="file" name="cbrfile">
 <input type="submit" name="submit" value="Upload">
 </form>
