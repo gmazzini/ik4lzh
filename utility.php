02d313b6f164b8fd455c4ec2b2f643867d43f31b
Gianluca Mazzini <gianluca@mazzini.org>
2021-01-28 23:22:05 +0100

    Update utility.php

--- utility.php
+++ utility.php
@@ -1,5 +1,5 @@
 <?php
-// v6 by IK4LZH 20210127
+// v7 by IK4LZH 20210128
 
 $bb=array(1=>160,3=>80,5=>60,7=>40,10=>30,14=>20,18=>17,21=>15,24=>12,28=>10,29=>10);
 
@@ -51,116 +51,85 @@ function mybreakdown($contest,$call,$datacontest,$aqso,$apoint,$amult){
   fclose(fp);
 }
 
-function mypar($str,$start,$len){
-  $aux=substr($str,$start-1,$len);
-  $mypos=strpos($aux,":");
-  return trim(substr($aux,0,$mypos));
-}
-
 function findcall($a){
-  global $myt,$zz;
+  global $z1,$z2,$z3;
   $call=strtoupper($a);
+  if(isset($z2[$call]))return $z1[$z2[$call]];
   $lc=strlen($call);
   $s=-1;
   for($q=1;$q<=$lc;$q++){
-    if(isset($myt[substr($call,0,$q)]))$s=$myt[substr($call,0,$q)];
+    if(isset($z3[substr($call,0,$q)]))$s=$z3[substr($call,0,$q)];
   }
-  if($s!=-1)return $zz[$s];
+  if($s!=-1)return $z1[$s];
 }
 
 $j=0;
-$hh=fopen("cty.dat","r");
+$hh=fopen("cty.csv","r");
 while(!feof($hh)){
-  $line=fgets($hh);
-  if(substr($line,0,4)=="    "){
-    $mll=$mll.trim(substr($line,4));
-  }
-  else {
-    $mll="";
-    $cqzone=mypar($line,27,5);
-    $ituzone=mypar($line,32,5);
-    $cont=mypar($line,37,5);
-    $call=mypar($line,70,6);
-    $name=mypar($line,1,26);
-  }
-  $tq2=strpos($mll,";");
-  if($tq2!==false){
-    $tq1=0;
-    $tq3=strlen($mll);
-    $mll[$tq2]=",";
-    for(;;){
-      if($tq1>=$tq3)break;
-      $tq2=strpos($mll,",",$tq1);
-      $v=substr($mll,$tq1,$tq2-$tq1);
-      $tq1=$tq2+1;
-      $to1=strpos($v,"=");
-      if($to1!==false && $to1==0){
-        $v=substr($v,1);
-      }
-      $to1=strpos($v,"(");
-      if($to1!==false){
-        $to2=strpos($v,")");
-        $cqzone=(int)substr($v,$to1+1,$to2-$to1-1);
-        $v=substr($v,0,$to1).substr($v,$to2+1);
-      }
-      $to1=strpos($v,"[");
-      if($to1!==false){
-        $to2=strpos($v,"]");
-        $ituzone=(int)substr($v,$to1+1,$to2-$to1-1);
-        $v=substr($v,0,$to1).substr($v,$to2+1);
-      }
-      $to1=strpos($v,"<");
-      if($to1!==false){
-        $to2=strpos($v,">");
-        $v=substr($v,0,$to1).substr($v,$to2+1);
-      }
-      $to1=strpos($v,"{");
-      if($to1!==false){
-        $to2=strpos($v,"}");
-        $cont=substr($v,$to1+1,$to2-$to1-1);
-        $v=substr($v,0,$to1).substr($v,$to2+1);
-      }
-      $to1=strpos($v,"~");
-      if($to1!==false){
-        $to2=strpos($v,"~",$to1+1);
-        $v=substr($v,0,$to1).substr($v,$to2+1);
-      }
-      $zz[$j]["prefix"]=$v;
-      $zz[$j]["base"]=$call;
-      $zz[$j]["cqzone"]=(int)$cqzone;
-      $zz[$j]["ituzone"]=(int)$ituzone;
-      $zz[$j]["cont"]=$cont;
-      $zz[$j]["name"]=$name;
-      $j++;
-      $lp=strpos($call,"/");
-      if($lp!==false){
-        $aux=$call;
-        $aux[$lp]="|";
-        $zz[$j]["prefix"]=$aux;
-        $zz[$j]["base"]=$call;
-        $zz[$j]["cqzone"]=(int)$cqzone;
-        $zz[$j]["ituzone"]=(int)$ituzone;
-        $zz[$j]["cont"]=$cont;
-        $zz[$j]["name"]=$name;
-        $j++;
-      }
-    }
-  }
-} 
-fclose($hh);
+  $dd=fgetcsv($hh,100000);
+  if($dd===FALSE)continue;
+  $ee=explode(" ",substr($dd[9],0,-1));
+  foreach($ee as $ff){
+    $z1[$j]["base"]=$dd[0];
+    $z1[$j]["name"]=$dd[1];
+    $z1[$j]["dxcc"]=$dd[2];
 
-for($i=0;$i<$j;$i++){
-  $qq=$zz[$i]["prefix"];
-  $ll=strpos($qq,"|");
-  if($ll!==false){
-    $pre=substr($qq,0,$ll);
-    $post=strtoupper(substr($qq,$ll+1));
-    $myt[$pre.$post]=$i;
-    for($w1=48;$w1<58;$w1++)$myt[$pre.chr($w1).$post]=$i;
-    for($w1=48;$w1<58;$w1++)for($w2=48;$w2<58;$w2++)$myt[$pre.chr($w1).chr($w2).$post]=$i;
+    $to1=strpos($ff,"(");
+    if($to1!==false){
+      $to2=strpos($ff,")",$to1+1);
+      $z1[$j]["cqzone"]=(int)substr($ff,$to1+1,$to2-$to1-1);
+      $ff=substr($ff,0,$to1).substr($ff,$to2+1);
+    }
+    else $z1[$j]["cqzone"]=(int)$dd[4];
+
+    $to1=strpos($ff,"[");
+    if($to1!==false){
+      $to2=strpos($ff,"]",$to1+1);
+      $z1[$j]["ituzone"]=(int)substr($ff,$to1+1,$to2-$to1-1);
+      $ff=substr($ff,0,$to1).substr($ff,$to2+1);
+    }
+    else $z1[$j]["ituzone"]=(int)$dd[5];
+
+    $to1=strpos($ff,"{");
+    if($to1!==false){
+      $to2=strpos($ff,"}",$to1+1);
+      $z1[$j]["cont"]=substr($ff,$to1+1,$to2-$to1-1);
+      $ff=substr($ff,0,$to1).substr($ff,$to2+1);
+    }
+    else $z1[$j]["cont"]=$dd[3];
+    
+    $to1=strpos($ff,"<");
+    if($to1!==false){
+      $to2=strpos($ff,">",$to1+1);
+      $ff=substr($ff,0,$to1).substr($ff,$to2+1);
+    }
+
+    $to1=strpos($ff,"~");
+    if($to1!==false){
+      $to2=strpos($ff,"~",$to1+1);
+      $ff=substr($ff,0,$to1).substr($ff,$to2+1);
+    }
+
+    if($ff[0]=="="){
+      $z2[substr($ff,1)]=$j;
+    }
+
+    $to1=strpos($ff,"/");
+    if($to1!==false){
+      $pre=substr($ff,0,$to1);
+      $post=strtoupper(substr($ff,$to1+1));
+      $z3[$pre.$post]=$j;
+      for($w1=48;$w1<58;$w1++)$z3[$pre.chr($w1).$post]=$j;
+      for($w1=48;$w1<58;$w1++)for($w2=48;$w2<58;$w2++)$z3[$pre.chr($w1).chr($w2).$post]=$j;
+    }
+
+    $z1[$j]["prefix"]=$ff;
+    $z3[$ff]=$j;
+
+    $j++;
   }
-  else $myt[$qq]=$i;
 }
+fclose($hh);
 
 for($i1=0;$i1<18;$i1++){
   for($j1=0;$j1<18;$j1++){
