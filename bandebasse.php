db74a1af9dc787422e892c146110a2b183c6f142
Gianluca Mazzini <gianluca@mazzini.org>
2021-01-30 22:14:34 +0100

    Update bandebasse.php

--- bandebasse.php
+++ bandebasse.php
@@ -1,5 +1,5 @@
 <?php
-// v6 by IK4LZH 20210122
+// v7 by IK4LZH 20210130
 include("utility.php");
 
 if(isset($_FILES['cbrfile']['tmp_name']))$hh=fopen($_FILES['cbrfile']['tmp_name'],"r");
@@ -7,11 +7,13 @@ else $hh=fopen("php://stdin","r");
 while(!feof($hh)){
   $line=fgets($hh);
   if(substr($line,0,4)!="QSO:")continue;
-  $parts=mysep($line,0);
+  $parts=mysep($line,10);
+  if(is_number($parts[8]))$qq=1;
+  $qq=0;
   
   $mytt=$parts[3].":".$parts[4];
   $band=$bb[floor($parts[1]/1000)];
-  $myid=$band."-".$parts[2]."-".$parts[8];
+  $myid=$band."-".$parts[2]."-".$parts[8+$qq];
   if(!isset($qso[$myid])){
     $qso[$myid]=1;
     if(!isset($aqso[$mytt]))$aqso[$mytt]=1;
@@ -19,7 +21,7 @@ while(!feof($hh)){
   }
   if($parts[2]=="PH")$pp=1;
   else if($parts[2]=="CW")$pp=2;
-  $aux=substr($parts[8],0,2);
+  $aux=substr($parts[8+$qq],0,2);
   if($aux=="IQ"||$aux=="IY")$pp=10;
   if(!isset($point[$myid])){
     $point[$myid]=$pp;
@@ -27,15 +29,14 @@ while(!feof($hh)){
     else $apoint[$mytt]+=$pp;
   }
  
-  $myid=$band."-".$parts[2]."-".substr($parts[10],0,2);
+  $myid=$band."-".$parts[2]."-".substr($parts[10+$qq],0,2);
   if(!isset($mult[$myid])){
     $mult[$myid]=1;
     if(!isset($amult[$mytt]))$amult[$mytt]=1;
     else $amult[$mytt]++;
   }
-  $mdxc=(int)substr($parts[10],3);
-  if($mdxc>0){
-    $myid=$band."-".$parts[2]."!".$mdxc;
+  if(isset($parts[11+$qq])){
+    $myid=$band."-".$parts[2]."!".(int)$parts[11+$qq];
     if(!isset($mult[$myid])){
       $mult[$myid]=1;
       if(!isset($amult[$mytt]))$amult[$mytt]=1;
