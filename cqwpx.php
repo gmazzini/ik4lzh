8fc102c353aad5ef09e02fb1f828d0ea90b869a5
Gianluca Mazzini <gianluca@mazzini.org>
2021-04-09 17:16:57 +0200

    Update cqwpx.php

--- cqwpx.php
+++ cqwpx.php
@@ -38,7 +38,6 @@ while(!feof($hh)){
     else $pp=2;
   }
   else $pp=1;
-  echo "<pre>".$parts[8]." ".$band." ".$pp."\n";
   if(!isset($point[$myid])){
     $point[$myid]=$pp;
     if(!isset($apoint[$mytt]))$apoint[$mytt]=$pp;
