dce9fd78ce05df79a0eaa6cde4eab7546b594899
Gianluca Mazzini <gianluca@mazzini.org>
2021-12-26 09:52:20 +0100

    Update ontimes.php

--- ontimes.php
+++ ontimes.php
@@ -1,6 +1,9 @@
 <?php
 // v1 by IK4LZH 20211226
 
+
+echo "<pre>";
+
 include("utility.php");
 $base=1;
 if(isset($_FILES['cbrfile']['tmp_name']))$hh=fopen($_FILES['cbrfile']['tmp_name'],"r");
@@ -10,7 +13,7 @@ while(!feof($hh)){
   if(substr($line,0,4)!="QSO:")continue;
   $parts=mysep($line,10);
   
-  $mytt=$parts[3].":".$parts[4];
+  $mytt=strtotime($parts[3]." ".$parts[4].":00");
 
   echo $mytt;
   
