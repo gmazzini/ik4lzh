437f688ebdc3c1a004e31192b03e8f4ff1186deb
Gianluca Mazzini <gianluca@mazzini.org>
2023-04-07 14:42:31 +0200

    Update qsorate.php

--- qsorate.php
+++ qsorate.php
@@ -2,7 +2,7 @@
 // v1 by IK4LZH 20230407
 
 include("utility.php");
-$ntqso=0;
+$nqso=0;
 if(isset($_FILES['cbrfile']['tmp_name']))$hh=fopen($_FILES['cbrfile']['tmp_name'],"r");
 else $hh=fopen("php://stdin","r");
 while(!feof($hh)){
@@ -10,16 +10,21 @@ while(!feof($hh)){
   if(substr($line,0,4)!="QSO:")continue;
   $parts=mysep($line,10);
   $aux=strtotime($parts[3]." ".substr($parts[4],0,2).":".substr($parts[4],2,2).":00");
-  if($aux<>$mytt[$ntqso]){
-    $mytt[$ntqso]=$aux;
-    $ntqso++;
-  }
+  $nqso++;
 }
 fclose($hh);
 sort($mytt);
-
-echo "<pre>\n\n$ntqso\n";
+$ntqso=1;
+$myt[0]=$mytt[0];
 for($n=1;$n<$ntqso;$n++){
+  if($mytt[$n]>$myt[$ntqso-1]){
+    $myt[$ntqso]=$mytt[$n];
+    $ntqso++;
+  }
+}
+
+echo "<pre>\n$nqso $ntqso\n";
+for($n=0;$n<$ntqso;$n++){
   $q=0;
   for($w=$n+1;$w<$ntqso;$w++){
     if($mytt[$w]>$mytt[$n]+3600)break;
