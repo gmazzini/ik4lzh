a2df511b60bfb9c0208d5b4f0b40dbca4d934743
Gianluca Mazzini <gianluca@mazzini.org>
2023-04-07 14:27:55 +0200

    Update qsorate.php

--- qsorate.php
+++ qsorate.php
@@ -2,23 +2,26 @@
 // v1 by IK4LZH 20230407
 
 include("utility.php");
-$nqso=0;
+$ntqso=0;
 if(isset($_FILES['cbrfile']['tmp_name']))$hh=fopen($_FILES['cbrfile']['tmp_name'],"r");
 else $hh=fopen("php://stdin","r");
 while(!feof($hh)){
   $line=fgets($hh);
   if(substr($line,0,4)!="QSO:")continue;
   $parts=mysep($line,10);
-  $mytt[$nqso]=strtotime($parts[3]." ".substr($parts[4],0,2).":".substr($parts[4],2,2).":00");
-  $nqso++;
+  $aux=strtotime($parts[3]." ".substr($parts[4],0,2).":".substr($parts[4],2,2).":00");
+  if($aux<>$mytt[$ntqso]){
+    $mytt[$ntqso]=$aux;
+    $ntqso++;
+  }
 }
 fclose($hh);
 sort($mytt);
 
 echo "<pre>";
-for($n=1;$n<$nqso;$n++){
+for($n=1;$n<$ntqso;$n++){
   $q=0;
-  for($w=$n+1;$w<$nqso;$w++){
+  for($w=$n+1;$w<$ntqso;$w++){
     if($mytt[$w]>$mytt[$n]+3600)break;
     else $q++;
   }
