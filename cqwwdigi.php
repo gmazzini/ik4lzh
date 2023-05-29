b1593c2e0d36bc3ebf105e6611e595b2d694f3b0
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-29 11:16:08 +0200

    Update cqwwdigi.php

--- cqwwdigi.php
+++ cqwwdigi.php
@@ -2,7 +2,8 @@
 // v5 by IK4LZH 20210129
 
 include("utility.php");
-if(isset($_FILES['cbrfile']['tmp_name']))$hh=fopen($_FILES['cbrfile']['tmp_name'],"r");
+if(isset($_GET['fromlog']))$hh=fopen($_GET['fromlog'],"r");
+elseif(isset($_FILES['cbrfile']['tmp_name']))$hh=fopen($_FILES['cbrfile']['tmp_name'],"r");
 else $hh=fopen("php://stdin","r");
 while(!feof($hh)){
   $line=fgets($hh);
