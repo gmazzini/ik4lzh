d237d3016d3e8a1404debf777eaa93a48cf2a214
Gianluca Mazzini <gianluca@mazzini.org>
2023-05-29 11:02:06 +0200

    Update aridx.php

--- aridx.php
+++ aridx.php
@@ -4,7 +4,8 @@
 $it=array("I","IT9","IS0");
 include("utility.php");
 $base=1;
-if(isset($_FILES['cbrfile']['tmp_name']))$hh=fopen($_FILES['cbrfile']['tmp_name'],"r");
+if(isset($_GET['fromlog']))$hh=fopen($_GET['fromlog'],"r");
+elseif(isset($_FILES['cbrfile']['tmp_name']))$hh=fopen($_FILES['cbrfile']['tmp_name'],"r");
 else $hh=fopen("php://stdin","r");
 while(!feof($hh)){
   $line=fgets($hh);
